093bf80146a4de012ce452284120abe7e9600fe5
Tamoziit <tamojitdas181007@gmail.com>
2024-11-15 21:38:29 +0530

    stdout flushing & CPU idle time handling

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ void table2(Pr *P, int n, int qt, int st, int switchCount, int tt);
 Pr *sortProcesses(Pr *arr, int n);
 void enQueue(QL **front, QL **rear, Pr val);
 Pr deQueue(QL **front);
-void *scheduler(void* threadargs);
+void *scheduler(void *threadargs);
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t turn_cond = PTHREAD_COND_INITIALIZER;
@@ -20,10 +20,11 @@ pthread_cond_t turn_cond = PTHREAD_COND_INITIALIZER;
 int tt, turn, qt, st, n, switchCount = 0, totalRemt = 0;
 QL *front = NULL, *rear = NULL;
 
-typedef struct {
-    Pr *currentProcess;
-    Pr *processArray;
-    int numProcesses;
+typedef struct
+{
+	Pr *currentProcess;
+	Pr *processArray;
+	int numProcesses;
 } ThreadArgs;
 
 /* Master Thread */
@@ -33,68 +34,75 @@ int main()
 	int i, status;
 	printf("Enter the no. of Processes\n");
 	scanf("%d", &n);
-	
-	P = (Pr*)malloc(n*sizeof(Pr));
+
+	P = (Pr *)malloc(n * sizeof(Pr));
 	pthread_t threads[n];
 	char buffer[5];
 	void *retval;
-	
-	for(i=0; i<n; i++)
+
+	for (i = 0; i < n; i++)
 	{
-		printf("Enter Arrival Time & Burst Time of Process no. %d\n", i+1);
+		printf("Enter Arrival Time & Burst Time of Process no. %d\n", i + 1);
 		scanf("%d%d", &P[i].at, &P[i].bt);
 		sprintf(buffer, "P%d", i + 1);
-        P[i].name = strdup(buffer);
-        P[i].remt = P[i].bt;
-        totalRemt += P[i].remt;
-        P[i].index = i+1;
+		P[i].name = strdup(buffer);
+		P[i].remt = P[i].bt;
+		totalRemt += P[i].remt;
+		P[i].index = i + 1;
 	}
-	
+
 	printf("Enter Time Quantum size\n");
 	scanf("%d", &qt);
 	printf("Enter Context Switching Time\n");
 	scanf("%d", &st);
-	
+
 	table1(P, n, qt, st);
-	
+
 	printf("Sorting Processes according to Arrival Time...\n");
 	P = sortProcesses(P, n);
 	printf("Sorted processes:\n");
-	for(i=0; i<n; i++)
+	for (i = 0; i < n; i++)
 		printf("%s:(%d); index = %d ", P[i].name, P[i].at, P[i].index);
 	printf("\n");
-	
+
 	/* Round Robin Init */
 	turn = P[0].index;
 	tt = P[0].at;
-	
 	printf("Initially; tt = %d\n\nEvaluating Gantt Chart...\n\n", tt);
-	
-	for(i=0; i<n; i++)
+
+	if (tt != 0)
 	{
-		ThreadArgs *args = (ThreadArgs*)malloc(sizeof(ThreadArgs));
+		for (i = 0; i < tt; i++)
+		{
+			printf(" X ");
+			switchCount++;
+		}
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		ThreadArgs *args = (ThreadArgs *)malloc(sizeof(ThreadArgs));
 		args->currentProcess = &P[i];
 		args->processArray = P;
 		args->numProcesses = n;
-		
-		if((status=pthread_create(&threads[i], NULL, &scheduler, (void*)args)))
+
+		if ((status = pthread_create(&threads[i], NULL, &scheduler, (void *)args)))
 		{
 			printf("Thread creation failed\n");
 			exit(0);
 		}
 	}
-	
-	
-	for(i=0; i<n; i++)
+
+	for (i = 0; i < n; i++)
 	{
 		pthread_join(threads[i], &retval);
 		sleep(1);
 	}
-	
+
 	printf("\n");
 	table2(P, n, qt, st, switchCount, tt);
-	printf("Final Time = %d\n", tt-st);
-	
+	printf("Final Time = %d\n", tt - st);
+
 	pthread_mutex_destroy(&mutex);
 	pthread_cond_destroy(&turn_cond);
 	free(P);
--- scheduler.c
+++ scheduler.c
@@ -11,7 +11,8 @@ extern pthread_cond_t turn_cond;
 extern int tt, turn, qt, st, switchCount, totalRemt;
 extern QL *front, *rear;
 
-typedef struct {
+typedef struct
+{
     Pr *currentProcess;
     Pr *processArray;
     int numProcesses;
@@ -20,10 +21,10 @@ typedef struct {
 void enQueue(QL **front, QL **rear, Pr val);
 Pr deQueue(QL **front);
 
-void *queueMaintainer(Pr* P, int n)
+void *queueMaintainer(Pr *P, int n)
 {
     int i;
-    
+
     for (i = 0; i < n; i++)
     {
         if (tt == P[i].at && P[i].enqueued == 0 && P[i].remt > 0)
@@ -34,10 +35,17 @@ void *queueMaintainer(Pr* P, int n)
     }
 }
 
-void advanceTimeDeadlockAvoider(Pr* processArray, int n) {
-    for (int i = 0; i < n; i++) {
-        if (processArray[i].cpu == 0 && processArray[i].at > tt) {
-        	switchCount += (processArray[i].at - tt);
+void advanceTimeDeadlockAvoider(Pr *processArray, int n)
+{
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        if (processArray[i].cpu == 0 && processArray[i].at > tt)
+        {
+            switchCount += (processArray[i].at - tt);
+            for (j = 0; j < (processArray[i].at - tt); j++)
+                printf(" X ");
+
             tt = processArray[i].at;
             turn = processArray[i].index;
             pthread_cond_broadcast(&turn_cond);
@@ -46,37 +54,41 @@ void advanceTimeDeadlockAvoider(Pr* processArray, int n) {
     }
 }
 
-void *scheduler(void* threadargs)
+void *scheduler(void *threadargs)
 {
-	ThreadArgs *args = (ThreadArgs*)threadargs;
+    ThreadArgs *args = (ThreadArgs *)threadargs;
     Pr *P = args->currentProcess;
     Pr *allProcesses = args->processArray;
     int n = args->numProcesses;
-    
-	pthread_t sid;
-	Pr temp;
-	int timeSlice = 0, i;
-	struct timespec ts;
+
+    pthread_t sid;
+    Pr temp;
+    int timeSlice = 0, i;
+    struct timespec ts;
     ts.tv_sec = 1;
-    ts.tv_nsec = 0; 
-	
-	while (1) {
+    ts.tv_nsec = 0;
+
+    while (1)
+    {
         pthread_mutex_lock(&mutex);
 
-        while (turn != P->index) {
-        	pthread_cond_wait(&turn_cond, &mutex);
+        while (turn != P->index)
+        {
+            pthread_cond_wait(&turn_cond, &mutex);
         }
 
-        if (P->remt > 0) {
+        if (P->remt > 0)
+        {
             sid = pthread_self();
             P->cpu = 1;
-            //printf("Turn = %d\n", turn);
 
-            while (timeSlice < qt && P->remt > 0) {
-            	if(P->remt == P->bt)
-            		P->start = tt;
+            while (timeSlice < qt && P->remt > 0)
+            {
+                if (P->remt == P->bt)
+                    P->start = tt;
                 tt++;
                 printf(" %s ", P->name);
+                fflush(stdout);
                 P->remt--;
                 totalRemt--;
                 timeSlice++;
@@ -84,37 +96,44 @@ void *scheduler(void* threadargs)
                 nanosleep(&ts, NULL);
             }
 
-            if (P->remt > 0) {
+            if (P->remt > 0)
+            {
                 enQueue(&front, &rear, *P);
             }
-            if(P->remt == 0)
-            	P->ct = tt;
+            if (P->remt == 0)
+                P->ct = tt;
 
-            for(i=1; i<=st; i++)
+            for (i = 1; i <= st; i++)
             {
-            	tt++;
-            	switchCount++;
-            	queueMaintainer(allProcesses, n);
+                tt++;
+                switchCount++;
+                queueMaintainer(allProcesses, n);
             }
-            if(totalRemt > 0)
-            	printf(" X ");
+            if (totalRemt > 0)
+            {
+                printf(" X ");
+                fflush(stdout);
+            }
 
             Pr temp = deQueue(&front);
-            if (temp.index != -1) {
+            if (temp.index != -1)
+            {
                 turn = temp.index;
-            } else {
+            }
+            else
+            {
                 advanceTimeDeadlockAvoider(allProcesses, n);
             }
-			//printf("Next turn = %d\n", turn);
-			pthread_cond_broadcast(&turn_cond);
+            pthread_cond_broadcast(&turn_cond);
         }
 
         pthread_mutex_unlock(&mutex);
         timeSlice = 0;
 
-        if (P->remt == 0) {
+        if (P->remt == 0)
+        {
             free(args);
-            pthread_exit((void*)(long)sid);
+            pthread_exit((void *)(long)sid);
         }
     }
 }
